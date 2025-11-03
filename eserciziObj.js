9d52669c09df50e25b202e2d7d19c588dbe90869
“valentinaottaviani33” <valentinaottaviani33@gmail.com>
2025-11-03 16:01:12 +0100

    primo commit

---
+++ eserciziObj.js
@@ -0,0 +1,174 @@
+//Creare un oggetto a vostra scelta
+
+//    -aggiungere una nuova proprietà
+//    -modificare una proprietà
+//    -eliminare una proprieà
+//    -aggiungere una funzione
+//    -richiamare la funzione
+
+let film = {
+
+    titolo: "Indiana Jones",
+    regista: "Tizio",
+    anno : 1988,
+    produzione: "USA"
+}
+
+//aggiungo proprietà
+film.cast = "Eroi";
+
+//modificare proprietà
+film.anno = 1990;
+
+//eliminare proprietà
+delete film.produzione;
+
+//aggiungere una funzione
+film.inizio = function() {
+
+    //console.log("Il film inizia alle ore 9");
+};
+
+//richiamo funzione
+film.inizio();
+
+//console.log(film);
+
+
+
+//ESERCIZIO : inserimento di studenti , calcolo media e miglior voto 
+
+// In questo programma, l utente dovrà inserire i dati di 3 studenti con prompt(), 
+// Nome, Cognome e Voto
+// Dovrà esser mostrato l elenco completo degli studenti,
+// Calcolare e stampare la media dei voti
+// Trovare lo studente con il voto piu alto e stampare lo studente migliore
+
+
+//Per 3 volte devo chiedere 3 dati : nome cognome e voto ( string, string, number(float))
+//Salvo i dati in un array di oggetti perchè sono 3 studenti con 3 proprietà
+
+
+//array vuoto della classe
+let classe = [];
+
+
+//chiedo all utente per 3 volte 3 dati
+for (i = 0; i < 3; i++){
+
+  let nome = prompt("Inserisci il tuo nome : ");
+  let cognome = prompt("Inserisci il cognome : ");
+  let voto = parseFloat(prompt("Inserisci il voto : "));
+
+  //creo l oggetto studente e lo aggiungo all array classe
+  let studente = {nome : nome, cognome : cognome, voto : voto }; 
+
+  //aggiungo studente all array classe
+  classe.push(studente);
+
+}
+
+//stampa dei risultati
+console.log("Elenco studenti : ");
+
+
+for (let i = 0; i < classe.length; i++){
+
+  console.log(classe[i].nome + classe[i].cognome + " - voto : " + classe[i].voto);
+    
+}
+
+//console.log(classe);
+
+//calcolo media
+
+//inizializzo contatore a zero, che sarà la somma dei voti
+let somma = 0;
+
+for (let i = 0; i < classe.length; i++){
+
+  somma += classe[i].voto; // somma = somma + voto
+
+}
+
+//Media
+let media = somma / classe.length;
+
+console.log("la media dei voti è : " + media.toFixed(1));
+
+
+//non conosco qual è il voto piu alto
+//ma se ipotizzo che il primo sia il migliore
+
+//trovo lo studente con il voto piu alto
+
+/*
+let votoMigliore = classe[0]; // il primo oggetto ha il voto piu alto
+
+for(let i = 1; i < classe.length; i++){
+
+  if (classe[i].voto > votoMigliore.voto){
+
+    votoMigliore = classe[i];
+
+  }
+}
+*/
+
+let votoMax = classe[0].voto; //assumo che il primo sia il migliore 
+let isMigliore = true; //imposto migliore a true
+
+//trovo voto massimo
+for (let i = 1; i < classe.length; i++){
+
+  if (classe[i].voto > votoMax){
+
+    votoMax = classe[i].voto;
+  
+  }
+
+}
+
+
+
+//controllo se esiste uno studente che ha lo stesso voto massimo
+for(let i = 0; i < classe.length; i++ ){
+
+  if(classe[i].voto === votoMax ){
+
+    //se piu' di uno ha lo stesso valore, non esiste il migliore
+    if (classe.filter(s => s.voto === votoMax).length > 1){
+
+      isMigliore = false;
+
+    }
+
+  }
+
+}
+
+if(isMigliore){
+
+  let migliore = classe.find(s => s.voto === votoMax);
+
+  console.log("Esiste un voto piu alto di tutti : " + votoMax);
+  console.log("Miglior studente : " + migliore.nome);
+
+}else {
+
+  console.log("nOn esiste un voto piu alto di tutti ( ci sono parità )");
+
+}
+
+console.log("media dei voti : " + media.toFixed(1));
+
+//IMPLEMENTARE CONTROLLO SE I VOTI SONO UGUALI, NON ESISTE IL MIGLIORE
+
+
+
+
+
+
+
+
+
---
+++ oggetti.js
@@ -0,0 +1,214 @@
+let a = "pippo"; //variabile di tipo stringa
+
+let b = 9; //variabile number
+
+let array = []; // array vuoto
+
+let c = false; // booleano
+
+
+//oggetto vuoto
+let oggetto = {};
+
+
+//---------------------------------OGGETTI--------------------------------
+
+
+let persona = {
+
+    nome : "Diego",
+    cognome : "Aneli",
+    eta : 43,
+    citta : "Parma",
+    sposato : false,
+    parla : function(){
+
+                console.log("sono una persona che parla");
+            
+            }
+}
+
+
+//accedere ai valori con la dot notation (.valore)
+//console.log("Nome dell oggetto persona : " + persona.nome);//nome
+//console.log("Cognome dell oggetto persona : " + persona.cognome);//cognome
+//console.log("Città dell oggetto persona : " + persona.citta);//città
+//console.log("Età dell oggetto persona : " + persona.eta);//età
+//console.log("Stato dell oggetto persona : " + persona.sposato);//stato
+
+
+//richiamo funzione parla dell oggetto persona
+
+persona.parla();
+
+
+//modifica di proprietà
+persona.eta = 35; //modifica
+persona.eta = 30; //piu giovane
+persona.sposato = true;
+
+//aggiungere una proprietà
+persona.professione = "Sviluppatore";
+
+
+//eliminare una proprietà
+delete persona.sposato;
+
+
+console.log("oggetto :" , persona);
+
+
+//STAMPARE CHIAVE CON CICLO "FOR...IN"
+let studente = {
+
+    nome : "Giulia",
+    corso : "Informatica",
+    anno : 3
+
+};
+
+for (let i in studente){
+
+    console.log(i + "->" + studente[i]);
+
+}
+
+//clonare o copiare un oggetto con metodo "spread operator"
+let copiaStudente = { ...studente };
+let copia2 = { ...studente };
+console.log(copiaStudente);
+
+//copiare un oggetto con metodo "assign"
+let copiaStudente2 = Object.assign({}, studente);
+
+//come restituire un array con tutte le chiavi
+console.log(Object.keys(studente));
+
+
+//come restituire in un array tutti i valori
+console.log(Object.values(studente));
+
+
+//come restituire un array di coppie [chiave, valore]
+console.log(Object.entries(studente));
+ 
+
+
+
+
+
+
+
+//---------------------- NIDIFICAZIONE -> OGGETTI DENTRO OGGETTI
+
+
+let scuola = {
+
+    nome : "Itis L.da Vinci",
+    
+    indirizzoDomicilio : {
+        via: "Via roma 1",
+        citta : "Roma"
+    },
+
+    indirizzoResidenza : {
+        via: "Via VERDI 38",
+        citta : "Parma"
+    }
+
+};
+
+console.log(scuola);
+console.log(scuola.indirizzoDomicilio.citta) //Roma
+console.log(scuola.indirizzoResidenza.citta) //Parma
+
+
+
+
+
+
+
+
+//----------------------NIDIFICAZIONE -> ARRAY CON DENTRO OGGETTI
+
+
+let persone = [
+
+                {nome: "Marco", eta: 18, citta: "Roma"}, //   0
+                {nome: "Mario", eta: 27, citta: "Milano"}, // 1
+                {nome: "Pietro", eta: 38, citta: "Pisa"} //   2
+              
+              ];
+
+//accesso oggetto 
+console.log(persone[0]); //primo oggetto
+console.log(persone[1]); //secondo oggetto
+console.log(persone[2]); //terzo oggetto
+
+//accesso proprietà specifica
+console.log(persone[0].nome); // stampo il nome del primo oggetto
+console.log(persone[2].citta); // Pisa
+
+//stampare con ciclo for tutti i dati
+for (let i = 0; i < persone.length; i++){
+
+    console.log(persone[i].nome + " vive a " + persone[i].citta );
+}
+
+//Aggiungere un nuovo oggetto nell array di oggetti
+persone.push({nome: "Diego", eta: 30, citta: "Udine", auto: true});
+
+console.log(persone);
+
+//come aggiungere una proprietà ad un singolo oggetto di un array di oggetti
+persone[3].professione = "Sviluppatore";
+console.log(persone[3]);
+
+//come modificare una proprietà ad un singolo oggetto di un array di oggetti
+persone[0].citta = "Milano";
+console.log(persone[3]);
+
+
+
+
+
+
+//---------------------------------FILTRARE O CERCARE NELL ARRAY DI OGGETTI
+
+let listaPersone = [
+
+        {nome: "Marco", eta: 18, citta: "Roma"}, //   0
+        {nome: "Mario", eta: 27, citta: "Milano"}, // 1
+        {nome: "Pietro", eta: 38, citta: "Pisa"} //   2
+];
+
+//mi salvo in una variabile il dato da filtrare
+let personaCercata = listaPersone.find(p => p.nome === "Mario");
+console.log(personaCercata);
+
+
+//filtrare per età (range di eta con operatore di confronto)
+let adulti = listaPersone.filter(p => p.eta > 25 && p.eta < 35  )
+console.log(adulti);
+
+
+
+
+
+
+//---------------------------------CONVERTIRE OGGETTI JSON E VICEVERSA----
+
+//Da obj in string
+let jsonString = JSON.stringify(persone);
+console.log( jsonString);
+
+
+//da jsonstring a obj
+let dati = JSON.parse(jsonString);
+console.log(dati[0].nome);
+
+console.log(typeof dati);
+
+
+//FORMATO .JSON -> JAVASCRIPT OBJECT NOTATION 
+//XML(VECCHIO FORMATO)
